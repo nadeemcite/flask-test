30bbbbd05fce84f74acda8da833558270dd8c62a
Nadeem Sajjad <nady5730642@gmail.com>
2022-02-05 19:13:19 +0530

    dfsdfs

--- index.py
+++ index.py
@@ -11,19 +11,22 @@ def home():
     if question:
         answer = "Answer"
     else:
-        openai.organization = "org-aiO2YqmcjKmGasfKm8oo8GYN"
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        openai.Engine.list()
-        resp = openai.Completion.create(
-            engine="text-davinci-001",
-            prompt=question,
-            max_tokens=5
-        )
-        print(resp["choices"])
-        print(resp["choices"][0])
-        if resp["choices"]:
-            print(resp["choices"][0]["text"])
-            answer = resp["choices"][0]["text"]
-        else:
-            answer = "Not found any answer"
+        try:
+            openai.organization = "org-aiO2YqmcjKmGasfKm8oo8GYN"
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+            openai.Engine.list()
+            resp = openai.Completion.create(
+                engine="text-davinci-001",
+                prompt=question,
+                max_tokens=5
+            )
+            print(resp["choices"])
+            print(resp["choices"][0])
+            if resp["choices"]:
+                print(resp["choices"][0]["text"])
+                answer = resp["choices"][0]["text"]
+            else:
+                answer = "Not found any answer"
+        except Exception as e:
+            print(e)
     return render_template("index.html", question=question, answer=answer)
